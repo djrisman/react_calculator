ae439a424fc87bf3afe5d9ab69c66f15bad978b2
risman arung barbarossa <rismanjaya1@gmail.com>
2018-12-31 23:51:05 +0100

    New Update

--- src/Components/Transition.jsx
+++ src/Components/Transition.jsx
@@ -13,9 +13,9 @@ render(){
     transitionName="fade"
     transitionEnterTimeout={300}
     transitionLeaveTimeout={300}>
-    {listing.map(list => {
+    {listing.map((list, index) => {
           if(list[1] !== "="){
-          return <li>{list[0]}{list[1]}{list[2]} = {list[3]} </li>
+          return <li key = {index}>{list[0]}{list[1]}{list[2]} = {list[3]} </li>
           }
       })}
     </ReactCSSTransitionGroup>
